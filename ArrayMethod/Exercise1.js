6ea87a5fc7890fd241c8e2e497e5957e30220ba8
Thyrak12 <thyrak9167@gmail.com>
2025-08-17 12:36:35 +0700

    practice

---
+++ ArrayMethod/Exercise1.js
@@ -0,0 +1,29 @@
+// Array Method
+
+// Exercise 1
+let numbers = [1, 2, 3, 4, 5];
+// TODO: Use map() to create a new array with each number multiplied by 2
+
+
+// Exercise 2
+let nums = [10, 15, 20, 25, 30];
+// TODO: Use filter() to keep only even numbers
+
+
+// Exercise 3
+let prices = [5, 10, 15, 20];
+// TODO: Use reduce() to calculate the total sum
+
+
+// Exercise 4
+let fruits = ["apple", "banana", "cherry", "mango"];
+// TODO: Use find() to get the fruit "cherry"
+// TODO: Use includes() to check if "banana" exists
+
+
+// Exercise 5
+let names = ["Zara", "Alex", "John", "Maya"];
+// TODO: Sort alphabetically
+// TODO: Reverse the order
+
+
